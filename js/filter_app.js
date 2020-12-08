1f58e5955b95d052669346b67b28945ecc853554
Pradyum Agarwal <72073629+Pradyum-19@users.noreply.github.com>
2020-12-08 15:07:38 +0530

    Update filter_app.js

--- js/filter_app.js
+++ js/filter_app.js
@@ -2,9 +2,9 @@ angular.module("MyApp",[])
 .controller("MyCtrl",function($scope)
 {
     var review=[
-        {title:'Mobile', type:'Review', location:'Bangalore', pricell:3000, priceul:6000, description:'Good'},
-        {title:'Laptop', type:'Review', location:'Delhi', pricell:3000, priceul:6000, description:'Great'},
-        {title:'Speaker', type:'Review', location:'Mumbai', pricell:3000, priceul:6000, description:'Best'}
+        {title:'Mobile', type:'Review', rname:'Pradyum', pricell:5000, priceul:60000, description:'Good'},
+        {title:'Laptop', type:'Review', rname:'Pradyum', pricell:30000, priceul:600000, description:'Great'},
+        {title:'Speaker', type:'Review',rname:'Pradyum', pricell:3000, priceul:6000, description:'Best'}
         ];
     console.log(review);
     $scope.review=review;
